1cc0be569c81072e93223f1428fc33312768c24f
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-28 14:02:55 +0530

    Using Functions

--- print in naatural numbers.c
+++ print in naatural numbers.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-#include<string.h>
+
+void printNaturalNumbers(int);
+
 int main() {
-     int n,i ;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    for(i=1;i<=n;i++){
-        
-            printf("%d ", i);
-    }
+    int n ;
+    printf("First  natural numbers:\n");
+    scanf("%d",&n);
+    printNaturalNumbers(n);
     return 0;
+}
+void printNaturalNumbers(int n)
+{
+    printf("First %d natural numbers:\n",n);
+    for(int i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+    printf("\n");
 }
